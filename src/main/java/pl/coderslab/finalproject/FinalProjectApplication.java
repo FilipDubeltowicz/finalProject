3e8ab50b7de4fd75e8b561f5590795c349dd2243
FilipDubeltowicz <f.dubeltowicz@gmail.com>
2023-04-20 23:14:21 +0200

    finalprject moved to correct package, AuthFilter removed

--- src/main/java/pl/coderslab/finalproject/FinalProjectApplication.java
+++ src/main/java/pl/coderslab/finalproject/FinalProjectApplication.java
@@ -1,4 +1,4 @@
-package pl.coderslab.finalproject.config;
+package pl.coderslab.finalproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
